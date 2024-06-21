4f640b2f3506b387d95fd19622467facaf79ee7a
Kangulungu <kangulungu@gmail.com>
2024-06-21 16:50:15 +0200

    .Net Core (Web application empty) folders structurs

---
+++ Folder_structure_And_Process_Flow/Folder_structure_And_Process_Flow/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Folder_structure_And_Process_Flow.Models;
+
+namespace Folder_structure_And_Process_Flow.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Folder_structure_And_Process_Flow/Folder_structure_And_Process_Flow/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Folder_structure_And_Process_Flow.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
