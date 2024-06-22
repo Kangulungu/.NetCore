e2741eeec1ce73d91deec855a8e0496f95823a3e
Kangulungu <kangulungu@gmail.com>
2024-06-22 12:07:24 +0200

    Reading Connectionstring by (Model class & IOptions)

---
+++ ConnectionString_Model_Class/ConnectionString_Model_Class/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ConnectionString_Model_Class.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace ConnectionString_Model_Class.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IOptions<MailConfig> _config;
+
+        public HomeController(ILogger<HomeController> logger, IOptions<MailConfig> config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+        public IActionResult Index()
+        {
+            string Email = _config.Value.mailFrom;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ConnectionString_Model_Class/ConnectionString_Model_Class/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ConnectionString_Model_Class.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
